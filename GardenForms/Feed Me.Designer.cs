472a1dac2c698f6c7c51cf555dac94fcaf110ce6
genkibijin2 <Kendall.Price@Euroglaze.Limited>
2025-04-11 15:04:31 +0100

    The Big Shake Update

--- GardenForms/Feed Me.Designer.cs
+++ GardenForms/Feed Me.Designer.cs
@@ -31,16 +31,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             btnClickThis = new Button();
             progressBar1 = new ProgressBar();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // btnClickThis
             // 
+            btnClickThis.Cursor = Cursors.Hand;
             btnClickThis.Font = new Font("CozetteCrossedSevenVector", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnClickThis.Location = new Point(12, 10);
             btnClickThis.Name = "btnClickThis";
             btnClickThis.Size = new Size(287, 73);
             btnClickThis.TabIndex = 0;
-            btnClickThis.Text = "Click this";
+            btnClickThis.Text = "Please Feed Me";
             btnClickThis.UseVisualStyleBackColor = true;
             btnClickThis.Click += btnClickThis_Click;
             // 
@@ -53,6 +56,16 @@
             progressBar1.Style = ProgressBarStyle.Continuous;
             progressBar1.TabIndex = 1;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.squiggle;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(313, 156);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 13F);
@@ -60,11 +73,13 @@
             ClientSize = new Size(313, 156);
             Controls.Add(progressBar1);
             Controls.Add(btnClickThis);
+            Controls.Add(pictureBox1);
             Font = new Font("CozetteCrossedSevenVector", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            FormBorderStyle = FormBorderStyle.Fixed3D;
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -72,5 +87,6 @@
 
         private Button btnClickThis;
         private ProgressBar progressBar1;
+        private PictureBox pictureBox1;
     }
 }
--- GardenForms/Feed Me.cs
+++ GardenForms/Feed Me.cs
@@ -3,12 +3,17 @@ namespace GardenForms
 {
     public partial class Form1 : Form
     {
+        //Initial Global Vars for the form
         int timesclicked = 0; //Sets the number of times the button has been clicked to the starting point of 0.
+        
+
+
         public Form1()
         {
             InitializeComponent();
             this.Text = "Feed Me";
-            progressBar1.Maximum = 100;
+            progressBar1.Maximum = 124;
+            btnClickThis.BackColor = Color.FromArgb(255, 255, 255);
         }
 
         private void btnClickThis_Click(object sender, EventArgs e)
@@ -25,11 +30,28 @@ namespace GardenForms
                 successJingle(); //plays the winning jingle when you hit exactly 100 clicks, but not afterwards.
                 btnClickThis.Text = "I'm full now, thanks."; 
             }
-            else if (timesclicked > 100) { //for when the amount of times clicked is more than 100.
+            else if ((timesclicked > 100) && (timesclicked < 120)) { //for when the amount of times clicked is more than 100.
                 btnClickThis.Text = "I'm full now, thanks.";
-
             }
-            else
+            else if (timesclicked == 120)
+            {
+                btnClickThis.Text = ("Honestly, stop");
+            }
+            else if ((timesclicked > 120) && (timesclicked <= 124))
+            {
+                btnClickThis.Text = btnClickThis.Text += ".";
+            }
+            else if (timesclicked > 124)
+            {
+                ShakeThisWindow(this);
+                Console.Beep(750, 10);
+                Console.Beep(750, 10);
+                Console.Beep(750, 10);
+               
+            
+            }
+
+            else //Thanks x2, 3, etc combo counter.
             {
                 btnClickThis.Text = ("Thanks x" + timesclicked);
             }
@@ -39,17 +61,11 @@ namespace GardenForms
         public void successJingle()
         {
             //frequencies
-            int C = 525;
-            int Cs = 554;
-            int D = 587;
-            int Ds = 622;
             int E = 659;
             int F = 698;
-            int Fs = 739;
             int G = 784;
             int Gs = 830;
             int A = 880;
-            int As = 932;
             int B = 987;
 
             //tempo
@@ -67,8 +83,20 @@ namespace GardenForms
             Console.Beep(Gs, T);
             Console.Beep(B, T);
         }
-       
 
+        private static void ShakeThisWindow(Form1 form)
+        {
+            var randomNumber= new Random(50); //generates a random number sequence
+            var originalLocation = form.Location; //grabs the original location of the window
+            int magnitude = 10;
+            for (int s = 0; s < 10; s++)
+            {
+                form.Location = new Point(originalLocation.X + randomNumber.Next(-magnitude, magnitude), originalLocation.Y + randomNumber.Next(-magnitude, magnitude)); 
+                //Takes original location and applies random x
+                System.Threading.Thread.Sleep(20);
+            }
+            form.Location = originalLocation; //resets position of form back to normal
+        }
        
 
        
